914325e37d897a7daee7485e33e61beaca9c0197
91jzhu <j1712032377@163.com>
2021-12-28 18:25:41 +0800

    dialog 支持呼出和❌关闭

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,12 +1,23 @@
 <template>
-  <Dialog/>
+  <Dialog v-model:visible="x"/>
+  <Button @click="toggle">dialog</Button>
 </template>
 
 <script lang="ts">
 import Dialog from "../lib/Dialog.vue";
+import Button from "../lib/Button.vue";
+import {ref} from "vue";
 export default {
   name: "DialogDemo",
-  components: {Dialog}
+  components: {Button, Dialog},
+  setup(){
+    const x=ref(false)
+    const toggle=()=>{
+      x.value=!x.value
+      console.log(x.value);
+    }
+    return {toggle,x}
+  }
 }
 </script>
 
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,25 +1,40 @@
 <template>
-  <div class="dialog-overlay"></div>
-  <div class="dialog-wrapper">
-    <div class="dialog">
-      <header>标题<span class="dialog-close"></span></header>
-      <main>
-        <p>第一行字</p>
-        <p>第二行字</p>
-      </main>
-      <footer>
-        <Button>OK</Button>
-        <Button loading>Cancel</Button>
-      </footer>
+  <template v-if="visible">
+    <div class="dialog-overlay"></div>
+    <div class="dialog-wrapper">
+      <div class="dialog">
+        <header>标题<span class="dialog-close" @click="toggle"></span></header>
+        <main>
+          <p>第一行字</p>
+          <p>第二行字</p>
+        </main>
+        <footer>
+          <Button>OK</Button>
+          <Button loading>Cancel</Button>
+        </footer>
+      </div>
     </div>
-  </div>
+  </template>
 </template>
 
 <script lang="ts">
 import Button from './Button.vue'
+
 export default {
   name: "Dialog",
-  components:{Button}
+  components: {Button},
+  props: {
+    visible: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup(props,context){
+    const toggle=()=>{
+      context.emit('update:visible',!props.visible)
+    }
+    return {toggle}
+  }
 }
 </script>
 
@@ -32,6 +47,7 @@ $border-color: #d9d9d9;
   box-shadow: 0 0 3px fade_out(black, 0.5);
   min-width: 15em;
   max-width: 90%;
+
   &-overlay {
     position: fixed;
     top: 0;
@@ -41,6 +57,7 @@ $border-color: #d9d9d9;
     background: fade_out(black, 0.5);
     z-index: 10;
   }
+
   &-wrapper {
     position: fixed;
     left: 50%;
@@ -48,7 +65,8 @@ $border-color: #d9d9d9;
     transform: translate(-50%, -50%);
     z-index: 11;
   }
-  >header {
+
+  > header {
     padding: 12px 16px;
     border-bottom: 1px solid $border-color;
     display: flex;
@@ -56,20 +74,24 @@ $border-color: #d9d9d9;
     justify-content: space-between;
     font-size: 20px;
   }
-  >main {
+
+  > main {
     padding: 12px 16px;
   }
-  >footer {
+
+  > footer {
     border-top: 1px solid $border-color;
     padding: 12px 16px;
     text-align: right;
   }
+
   &-close {
     position: relative;
     display: inline-block;
     width: 16px;
     height: 16px;
     cursor: pointer;
+
     &::before,
     &::after {
       content: '';
@@ -80,9 +102,11 @@ $border-color: #d9d9d9;
       top: 50%;
       left: 50%;
     }
+
     &::before {
       transform: translate(-50%, -50%) rotate(-45deg);
     }
+
     &::after {
       transform: translate(-50%, -50%) rotate(45deg);
     }
