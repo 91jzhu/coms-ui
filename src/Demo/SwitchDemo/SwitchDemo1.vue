5169603fde60543eafc1e2ff5b1cce0db634446a
91jzhu <j1712032377@163.com>
2021-12-30 15:45:49 +0800

    抽离用户需要拷贝的代码

---
+++ src/Demo/SwitchDemo/SwitchDemo1.vue
@@ -0,0 +1,20 @@
+<template>
+  <Switch v-model:value="bool1"></Switch>
+</template>
+
+<script lang="ts">
+import Switch from '../../lib/Switch.vue'
+import {ref} from "vue";
+export default {
+  name: "SwitchDemo1",
+  components:{Switch},
+  setup(){
+    const bool1=ref(false)
+    return {bool1}
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,24 +1,80 @@
 <template>
-  <Switch v-model:value="bool" type="left" :func="onClick"/>
+  <div>
+    <h1>Switch 组件示例 </h1>
+    <div class="demo">
+      <h2>常规用法</h2>
+      <div class="demo-component">
+        <Switch v-model:value="bool1"/>
+      </div>
+      <div class="demo-actions">
+        <Button>查看代码</Button>
+      </div>
+      <SwitchDemo1/>
+      <div class="demo-code">
+        <pre>&lt;Switch v-model:value="bool1" /&gt;</pre>
+      </div>
+    </div>
+    <div class="demo">
+      <h2>支持 disabled </h2>
+      <div class="demo-component">
+        <Switch v-model:value="bool2" disabled/>
+      </div>
+      <div class="demo-actions">
+        <Button>查看代码</Button>
+      </div>
+      <SwitchDemo2/>
+      <div class="demo-code">
+        <pre>&lt;Switch v-model:value="bool2" disabled /&gt;</pre>
+      </div>
+    </div>
 </template>
 
 <script lang="ts">
 import Switch from '../lib/Switch.vue'
+import Button from '../lib/Button.vue'
+import SwitchDemo1 from '../Demo/SwitchDemo/SwitchDemo1.vue'
+import SwitchDemo2 from '../Demo/SwitchDemo/SwitchDemo2.vue'
+
 import {ref} from "vue";
+
 export default {
   name: "SwitchDemo",
-  components: { Switch},
-  setup(){
-    const bool=ref(false)
-    const onClick=()=>{
-      console.log('变化了')
-    }
-    return {bool,onClick}
+  components: {SwitchDemo1, Switch,Button,SwitchDemo2},
+  setup() {
+    const bool1 = ref(false)
+    const bool2 = ref(false)
+
+    return {bool1, bool2,}
   }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+$border-color: #d9d9d9;
+.demo {
+  border: 1px solid $border-color;
+  margin: 16px 0 32px;
+  >h2 {
+    font-size: 20px;
+    padding: 8px 16px;
+    border-bottom: 1px solid $border-color;
+  }
+  &-component {
+    padding: 16px;
+  }
+  &-actions {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+  }
+  &-code {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+    >pre {
+      line-height: 1.1;
+      font-family: Consolas, 'Courier New', Courier, monospace;
+      margin: 0;
+    }
+  }
+}
 
 </style>
