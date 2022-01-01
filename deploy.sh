2da89a0eabc3b9f12fabc35638d57e53457a414a
91jzhu <j1712032377@163.com>
2022-01-01 12:26:21 +0800

    一键部署

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
-rm -rf dist
-yarn build
-cd dist
-git init
-git add ./
-git commit -m 'deploy'
-bit branch -M master
-git remote add origin git@github.com:91jzhu/coms-ui-website.git
-git push -f -u origin master
+rm -rf dist &&
+yarn build &&
+cd dist &&
+git init &&
+git add ./ &&
+git commit -m 'deploy' &&
+bit branch -M master &&
+git remote add origin git@github.com:91jzhu/coms-ui-website.git &&
+git push -f -u origin master &&
 cd -
